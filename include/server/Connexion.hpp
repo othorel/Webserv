ed9d50b7fbc726582199e7343adcdc9edccea4c8
lmatkows <lmatkows@student.42perpignan.fr>
2025-06-03 13:37:41 +0200

    test

---
+++ include/server/Connexion.hpp
@@ -0,0 +1,17 @@
+
+#ifndef CONNEXION_HPP
+# define CONNEXION_HPP
+
+class Connexion
+{
+	public:
+		Connexion();
+		~Connexion();
+		Connexion(const Connexion & toCopy);
+		Connexion & operator=(const Connexion & other);
+
+	private:
+
+};
+
+#endif
