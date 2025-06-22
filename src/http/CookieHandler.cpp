ad0013cad86b3076074a36ee29a855219bb4f0d2
chdonnat <donnatchris@live.fr>
2025-06-22 15:54:43 +0200

    ADD src/http/CookieHandler - MOD src/CGIHandler (removed timeout in the CGI handler)

---
+++ src/http/CookieHandler.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include "../../include/http/CookieHandler.hpp"
+#include "../../include/http/HttpResponse.hpp"
+#include "../../include/http/HttpRequest.hpp"
+#include "../../include/http/HttpUtils.hpp"
+#include "../../include/config/Location.hpp"
+
+void CookieHandler::handleCookie(
+	HttpResponse & response, const HttpRequest & request)
+{
+	if (request.hasHeader("cookie") && !request.getHeaderValue("cookie").empty())
+		return ;
+
+	std::string randomString  = generateRandomString(10);
+
+	std::string cookie =
+		"session_id=" + randomString + "; Path=/; HttpOnly";
+	
+	response.addHeader("Set-Cookie", cookie);
+}
+
+std::string CookieHandler::generateRandomString(size_t size)
+{
+	static bool seeded = false;
+	if (!seeded) {
+		std::srand(std::time(NULL));
+		seeded = true;
+	}
+	std::string result;
+	for (size_t i = 0; i < size; ++i) {
+		char letter = 'a' + std::rand() % 26;
+		result += letter;
+	}
+	return (result);
+}
