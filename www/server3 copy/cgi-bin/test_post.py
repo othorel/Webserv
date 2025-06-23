afa12231d629f66f2422c7f19d67043e825c2fa7
Olivier Thorel <olthorel@student.42perpignan.fr>
2025-06-23 16:31:07 +0200

    add .php

--- www/server3 copy/cgi-bin/test_post.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys
-import os
-
-print("Content-Type: text/plain\n")
-print("Hello from POST script!")
-print("Content-Length:", os.environ.get("CONTENT_LENGTH"))
-print("Request Body:", sys.stdin.read())
